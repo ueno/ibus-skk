68bf8623776ab1c707a1c8b07bc7ce050c68f40a
Daiki Ueno <ueno@unixuser.org>
2009-11-09 03:15:33 +0900

    Use gnome-autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
-set -e
-set -x
+# Run this to generate all the initial makefiles, etc.
 
-autopoint --force
-libtoolize --automake --copy --force
-aclocal -I m4 --force
-autoheader --force
-automake --add-missing --copy --force
-autoconf --force
-export CFLAGS="-Wall -g -O0 -Wl,--no-undefined"
-export CXXFLAGS="$CFLAGS"
-./configure --enable-maintainer-mode $*
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="ibus-skk"
+
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/README ) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
+
+ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I m4"
+REQUIRED_AUTOMAKE_VERSION=1.8
+
+. gnome-autogen.sh
