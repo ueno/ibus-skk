eb2a6bdbfff79c47fdeb640cdaaf8bccc6bb3109
Daiki Ueno <ueno@unixuser.org>
2009-11-09 10:25:11 +0900

    Fix.

--- setup/config.py
+++ setup/config.py
@@ -6,24 +6,25 @@ class Config:
                        '/usr/share/skk/SKK-JISYO.L',
                        '/usr/local/share/skk/SKK-JISYO',
                        '/usr/local/share/skk/SKK-JISYO.L')
-    __usrdict_path = '~/.skk-ibus-jisyo'
+    __usrdict_path_unexpanded = '~/.skk-ibus-jisyo'
 
     def __init__(self, bus=ibus.Bus()):
         self.__bus = bus
         self.__config = self.__bus.get_config()
         
     def __sysdict_path(self):
-        for path in __sysdict_paths:
+        for path in self.__sysdict_paths:
             if os.path.exists(path):
                 return path
-    sysdict_path = property(lambda self: self.get_value('sysdict') or \
-                                self.__sysdict_path())
+    sysdict_path = property(lambda self: self.get_value(\
+            'sysdict', self.__sysdict_path()))
 
     def __usrdict_path(self):
-        os.path.expanduser(__usrdict_path)
+        usrdict_path = os.path.expanduser(self.__usrdict_path_unexpanded)
         open(usrdict_path, 'a+').close()
-    usrdict_path = property(lambda self: self.get_value('usrdict') or \
-                                self.__usrdict_path())
+        return usrdict_path
+    usrdict_path = property(lambda self: self.get_value(\
+            'usrdict', self.__usrdict_path()))
 
     def get_value(self, name, defval=None):
         value = self.__config.get_value('engine/SKK', name, None)
@@ -32,5 +33,6 @@ class Config:
         self.set_value(name, defval)
         return defval
 
-    def set_value(self, name, val):
-        self.__config.set_value('engine/SKK', name, val)
+    def set_value(self, name, value):
+        if value is not None:
+            self.__config.set_value('engine/SKK', name, value)
