feaa3d5816c1d92f9f55d16caf740a2ee2f5d6fa
Daiki Ueno <ueno@unixuser.org>
2011-05-09 18:07:38 +0900

    Make sure to save sysdict_paths when set (Issue#31).

--- setup/config.py
+++ setup/config.py
@@ -29,6 +29,9 @@ class Config:
         'egg_like_newline': True,
         'use_nicola': False,
         }
+    # sysdict_paths needs special treatment since IBusConfig does not
+    # allow empty arrays (Issue#31).
+    __keys = __defaults.keys() + ['sysdict_paths']
 
     # Options which can only be specified in ~/.config/ibus-skk.json.
     # This is a workaround for that currently IBus does not allows
@@ -60,7 +63,7 @@ class Config:
                 self.__modified[name] = value
 
     def commit_all(self):
-        for name in self.__defaults.keys():
+        for name in self.__keys:
             value = self.__modified.get(name)
             if value is not None:
                 # print 'set_value engine/SKK/%s' % name
